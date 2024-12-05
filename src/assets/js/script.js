f23b3ef6362df97ce1a6c1266b8fb1db5ce8e92e
*melokow * <jenjamiob@gmail.com>
2024-12-05 12:23:06 +0100

    cambiosTablet y posicion carpetas

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -9,6 +9,27 @@ document.addEventListener("DOMContentLoaded", function() {
             .catch(error => console.error('Error cargando el archivo:', error));
     }
    
-    loadHTML('src/partials/_header.html', 'header-placeholder');
-    loadHTML('src/partials/_footer.html', 'footer-placeholder');
+    loadHTML('partials/_header.html', 'header-placeholder');
+    loadHTML('partials/_footer.html', 'footer-placeholder');
+});
+function cambiarImagen(imagen) {
+    // Obtén la fuente de la imagen clickeada
+    const nuevaFuente = imagen.src;
+    
+    // Cambia la fuente de la imagen principal
+    document.getElementById('imagen-principal').src = nuevaFuente;
+  }
+
+
+// Obtener el botón de "Añadir al carrito" y el contador del carrito
+const carritoButton = document.querySelector('.carrito');
+const contadorUnidades = document.getElementById('cart-count');
+
+// Inicializamos el contador de productos
+let sumaProductos = 0;
+
+// Función para añadir productos al carrito
+carritoButton.addEventListener('click', function() {
+    sumaProductos++; // Aumentar la cantidad de productos
+    contadorUnidades.textContent = sumaProductos; // Actualizar el texto del contador
 });
